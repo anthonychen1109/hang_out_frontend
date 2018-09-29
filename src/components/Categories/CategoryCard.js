7f9c00871964308b3e0a7828a17dbfc45de50269
anthonychen1109 <chen.anthony.1109@gmail.com>
2018-09-28 22:42:55 -0400

    added styling to category cards

--- src/components/Categories/CategoryCard.js
+++ src/components/Categories/CategoryCard.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
 
 class CategoryCard extends Component {
+
+  state = {
+    hover: false
+  }
+
+  toggleHover = () => {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
   render() {
-    const background = {
-      backgroundImage: `url(${this.props.img})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
+    // const background = {
+    //   backgroundImage: `url(${this.props.img})`,
+    //   backgroundRepeat: 'no-repeat',
+    //   backgroundSize: 'cover'
+    // }
+    let background;
+    if (this.state.hover) {
+      background = {
+        backgroundImage: `url(${this.props.img})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        maxWidth: '100%',
+        transition: '.35s',
+        transitionTimingFunction: 'ease-in',
+        transform: 'scale(1.1)'
+      }
+    } else {
+      background = {
+        backgroundImage: `url(${this.props.img})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        transition: '.35s',
+        transitionTimingFunction: 'ease-in',
+        transform: 'scale(1)',
+        width: '100%'
+      }
     }
     return (
-      <div className="card">
+      <div className="card" onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
         <div className="card-img-top" style={background}>
         </div>
         <div className="card-body">
