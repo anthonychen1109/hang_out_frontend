e9f4a51e4f89ec8add7aaf716fa49c398e9ddbc1
anthonychen1109 <chen.anthony.1109@gmail.com>
2018-09-30 00:30:55 -0400

    added new sections to groupInfo

--- src/components/Groups/GroupInfo.js
+++ src/components/Groups/GroupInfo.js
@@ -5,7 +5,8 @@ import Navbar from '../Navbar/Navbar';
 
 const mapStateToProps = (state) => {
   return {
-    group: state.group
+    group: state.group,
+    events: state.events
   }
 }
 
@@ -26,10 +27,34 @@ class GroupInfo extends Component {
   }
 
   render() {
+    console.log(this.props.group.group.group_img);
     return (
       <div>
         <Navbar/>
-        {this.renderGroup()}
+        <hr/>
+        <div className="groupInfoFiller container">
+          <div className="groupInfoImage">
+            <img src={this.props.group.group.group_img} alt="group image"/>
+          </div>
+          <div className="groupInfoContent">
+            <h1>{this.props.group.group.name}</h1>
+            <p>{this.props.group.group.num_users} members</p>
+            <p>organized by {this.props.group.group.organizer_name}</p>
+          </div>
+        </div>
+        <hr/>
+        <div className="groupInfoAbout container">
+          <div className="groupInfoLeft">
+            <h1>What we're about</h1>
+            <p>{this.props.group.group.description}</p>
+          </div>
+          <div className="groupInfoRight">
+            <h1>Upcoming Hang Outs</h1>
+            <div>
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
