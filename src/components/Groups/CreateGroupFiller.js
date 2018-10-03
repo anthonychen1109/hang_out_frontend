3abd796c3466082bc72c2b401946223e935d1513
anthonychen1109 <chen.anthony.1109@gmail.com>
2018-10-03 15:33:46 -0400

    added navigation to create group

--- src/components/Groups/CreateGroupFiller.js
+++ src/components/Groups/CreateGroupFiller.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom';
 
 class CreateGroupFiller extends Component {
+
+  handleCategories = () => {
+    this.props.history.push({
+      pathname: '/home'
+    })
+  }
+
+  handleEvents = () => {
+    this.props.history.push({
+      pathname: '/events'
+    })
+  }
+
     render() {
         return (
           <div className="groupsFiller">
@@ -8,8 +23,32 @@ class CreateGroupFiller extends Component {
                   <div className="fillerDiv">
                     {
                       this.props.hasToken
-                      ? <h1>Start a new group!</h1>
-                      : <h1 className="animated shake">MUST BE LOGGED IN TO START A NEW GROUP</h1>
+                      ? <div>
+                          <h1>Start a new group!</h1>
+                            <Button
+                              inverted color="blue"
+                              onClick={this.handleCategories}>
+                              See Categories
+                            </Button>
+                            <Button
+                              inverted color="blue"
+                              onClick={this.handleEvents}>
+                              See Events
+                            </Button>
+                        </div>
+                      : <div>
+                          <h1 className="animated shake">MUST BE LOGGED IN TO START A NEW GROUP</h1>
+                            <Button
+                              inverted color="blue"
+                              onClick={this.handleCategories}>
+                              See Categories
+                            </Button>
+                            <Button
+                              inverted color="blue"
+                              onClick={this.handleEvents}>
+                              See Events
+                            </Button>
+                        </div>
                     }
                   </div>
               </div>
@@ -18,4 +57,4 @@ class CreateGroupFiller extends Component {
     }
 }
 
-export default CreateGroupFiller;
+export default withRouter(CreateGroupFiller);
